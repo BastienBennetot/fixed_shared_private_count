4a3a4f0b327fa2adc0e77c2fd1332a5f05348429
Bastien Bennetot <86835441+BastienBennetot@users.noreply.github.com>
2021-09-03 11:03:39 +0200

    Update app.R

--- app.R
+++ app.R
@@ -1,60 +1,55 @@
 library(shiny)
 ui <- fluidPage(titlePanel("Shiny App to compute fixed/share/private SNP count"),
-                    tabsetPanel(
-                    tabPanel("Input and Results", fluid = TRUE,
-                             sidebarLayout(
-
-                               sidebarPanel(helpText(tags$h3("Data format"),"Your data must have a specific format, a tab separated
-                   table with different columns: ",br(),"scaffold, position, reference_nucleotide, 
-                                        alternative_nucleotide, allele_count and number_of_sample"),
-                                            "To produce this kind of file from a vcf.gz you can use the following bash command:",br(),
-                                            "bcftools view -S population1_individual_list vcf_file.vcf.gz |bcftools query  -f '%CHROM\\t%POS\\t%REF\\t%ALT\\t%AC\\t%AN\\n' > population1_summary.txt",br(),
-                                            "The word before _summary.txt will be used as a name for each population so name your files accordingly",
-                                            fileInput(
-                                              inputId = "files", 
-                                              label = "Choose all summary files to upload at once", 
-                                              multiple = TRUE
-                                            )
-                               ),
-                      mainPanel(fluidPage(helpText("Once you uplodaded the data, it may take several minutes to show results just under this text"),
-                                                                                    # This one is linked by the id 'download'
-                                                                                    downloadButton('downloadtable',"Download the summary table"),
-                                                                                  fluidRow(column(5,dataTableOutput('dtotable')))
-                                       
-                                   
+                tabsetPanel(
+                  tabPanel("Input and Results", fluid = TRUE,
+                           sidebarLayout(
                              
-                           )
-                  ),
-                  tabPanel("Other informations", fluid = TRUE,
-                           tags$h1("How SNP are computed ?"), 
-                           "within population :",br(),
-                           "There is two information when we compare alleles from population A and B ",br(),
-                           "The SNP is said to be \"fixed\" if all samples from a population have the same allele",br(),
-                           "Between population:",br(),
-                           "The SNP is said \"snp\" if at least one sample have a different allele within a population",br(),
-                           "When we compare alleles present in both populations, they can have same allele content (same ref or alt or same combination of multiple alleles) then we declare them \"same\" ",br(),
-                           "If allele content is different between populations, then they are declared \"diff\" ",
-                           tags$h2("We can obtain these combinations"),
-                           "fixed fixed	same	=>IDENTIC",br(),
-                           "snp snp	same	=>SHARED",br(),
-                           "fixed snp	same	=>NOT POSSIBLE",br(),
-                           "snp fixed	same	=>NOT POSSIBLE",br(),
-                           
-                           "fixed fixed	diff	=>FIXED",br(),
-                           "snp snp	diff	=>SHARED",br(),
-                           "fixed snp	diff	=>PRIVATE",br(),
-                           "snp fixed	diff	=>PRIVATE"
-                  ),
-                  #Add a tab to download the data
-                  tabPanel("Download the computed dataframe",fluidPage(helpText("You won't be able to see and download the data until the input is computed"),
-                    # This one is linked by the id 'download'
-                    downloadButton('download',"Download the data"),
-                    fluidRow(column(7,dataTableOutput('dto')))
-                  ))
-                  
-                  
-                ))
-
+                                     sidebarPanel(helpText(tags$h3("Data format"),"Your data must have a specific format, a tab separated
+                                                table with different columns: ",br(),"scaffold, position, reference_nucleotide, 
+                                                alternative_nucleotide, allele_count and number_of_sample"),
+                                                  "To produce this kind of file from a vcf.gz you can use the following bash command:",br(),
+                                                  "bcftools view -S population1_individual_list vcf_file.vcf.gz |bcftools query  -f '%CHROM\\t%POS\\t%REF\\t%ALT\\t%AC\\t%AN\\n' > population1_summary.txt",br(),
+                                                  "The word before _summary.txt will be used as a name for each population so name your files accordingly",
+                                                  fileInput(
+                                                    inputId = "files", 
+                                                    label = "Choose all summary files to upload at once", 
+                                                    multiple = TRUE)
+                                                  ),
+                                      mainPanel(fluidPage(helpText("Once you uplodaded the data, it may take several minutes to show results just under this text"),
+                                                 # This one is linked by the id 'download'
+                                                 downloadButton('downloadtable',"Download the summary table"),
+                                                 fluidRow(column(5,dataTableOutput('dtotable')))))
+                                        )
+                           ),
+                tabPanel("Other informations", fluid = TRUE,
+                                      tags$h1("How SNP are computed ?"), 
+                                      "within population :",br(),
+                                      "There is two information when we compare alleles from population A and B ",br(),
+                                      "The SNP is said to be \"fixed\" if all samples from a population have the same allele",br(),
+                                      "Between population:",br(),
+                                      "The SNP is said \"snp\" if at least one sample have a different allele within a population",br(),
+                                      "When we compare alleles present in both populations, they can have same allele content (same ref or alt or same combination of multiple alleles) then we declare them \"same\" ",br(),
+                                      "If allele content is different between populations, then they are declared \"diff\" ",
+                                      tags$h2("We can obtain these combinations"),
+                                      "fixed fixed	same	=>IDENTIC",br(),
+                                      "snp snp	same	=>SHARED",br(),
+                                      "fixed snp	same	=>NOT POSSIBLE",br(),
+                                      "snp fixed	same	=>NOT POSSIBLE",br(),
+                                      
+                                      "fixed fixed	diff	=>FIXED",br(),
+                                      "snp snp	diff	=>SHARED",br(),
+                                      "fixed snp	diff	=>PRIVATE",br(),
+                                      "snp fixed	diff	=>PRIVATE"
+                             ),
+                             #Add a tab to download the data
+              tabPanel("Download the computed dataframe",fluidPage(helpText("You won't be able to see and download the data until the input is computed"),
+                                                                                  # This one is linked by the id 'download'
+                                                                                  downloadButton('download',"Download the data"),
+                                                                                  fluidRow(column(7,dataTableOutput('dto')))
+                                                                   )
+                       )
+              )
+              )
 
 server <- function(input, output) {
   packages <- c("dplyr","stringr","tidyr")
